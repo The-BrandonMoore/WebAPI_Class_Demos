a81e231e212dfa545e1b87f0ba3a027c06857e9f
Brandon Moore <preacherman65@gmail.com>
2024-09-19 16:26:24 -0400

    first commit

---
+++ MoviesWebApi/MoviesWebApi/Controllers/MovieController.cs
@@ -0,0 +1,70 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        static List<string> Movies = new List<string> { "star wars", "Hello Kitty", "The Iron Giant", "Horse Movie" };
+
+        [HttpGet]
+        public List<string> GetAllMovies() { return Movies; }
+
+        [HttpGet("{idx}")]
+        public string GetMovieByIdx(int idx)
+        {
+            if (!ValidIndex(idx))
+            {
+                return "No Movie at that index position. Try again.";
+            }
+            else { return Movies[idx]; }
+        }
+
+        private bool ValidIndex(int idx)
+        {
+            bool valid = true;
+            if (idx < 0 || idx >= Movies.Count)
+            {
+                valid = false;
+            }
+            return valid;
+        }
+
+        [HttpPost]
+        public string AddMovie(string movie)
+        {
+            Movies.Add(movie);
+            return "Movie Added";
+        }
+
+        [HttpDelete("{idx}")]
+        public string RemoveMovie(int idx)
+        {
+            if (!ValidIndex(idx))
+            {
+                return "No movie at that index position";
+            }
+            else
+            {
+                Movies.RemoveAt(idx);
+                return "Movie Deleted";
+            }
+        }
+        [HttpPut]
+        public string UpdateMovie(int idx, string newTitle)
+        {
+            if (!ValidIndex(idx))
+            {
+                return "No movie at that index position";
+            }
+            else
+            {
+                Movies[idx] = newTitle;
+                return "Movie Updated";
+            }
+        }
+
+    }
+}
---
+++ MoviesWebApi/MoviesWebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "We are probably not going to make it", "Bracing", "Maybe", "Chilly", "Spicy", "Nuclear", "Cool", "This is NOT good", "Mild", "Warm", "Threatening", "Balmy", "I Call Bull Crap", "Hot", "Potato", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
